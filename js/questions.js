cee4fb27a0888e86ba7e043fcc6e097095171304
GOUTHAM429 <63777844+GOUTHAM429@users.noreply.github.com>
2021-07-02 17:45:56 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,123 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How shall the summer arise in joy, Or the summer fruits appear if _____.",
+    answer: "All of the Above",
+    options: [
+          "Buds are nipped",
+          "Blossoms are blown",
+          "Plants are stripped",
+          "All of the Above"
+    ]
+  },
+   
+   {
+    numb: 2,
+    question: "Who does he compare himself to when he says “How can a child when fears annoy, But droop his tender wing, And forget his youthful spring.?",
+    answer: "Caged bird",
+    options: [
+       "Book",
+       "Flower",
+       "Nip in the",
+       "Caged bird"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The little ones spend the day, in _____ and _____ ?",
+    answer: "Sighing, dismay",
+    options: [
+          "Sighing, dismay",
+          "Happiness, glory",
+          "Happiness, joy",
+          "Sighing, sorrow"
+    ]
+  },
+    
+    {
+    numb: 4,
+    question: "Who sings along with the school boy?",
+    answer: "Skylark",
+    options: [
+         "His parents",
+         "Huntsman",
+         "Skylark",
+         "His friends"
+    ]
+  },
+    {
+    numb: 5,
+    question: "I _____ to rise on a sunday morn?",
+    answer: "Love",
+    options: [
+          "Hate",
+          "Despise",
+          "Regret",      
+          "Love"
+    ]
+  },
+
+       {
+       numb: 6,
+       question: "Who is he talking to when he says “if buds are nip’d ?",
+       answer: "His parents",
+       options: [
+       "His parents",
+       "Teacher",
+       "Caged bird",
+       "All of the Above"
+     ]
+   },
+     
+      {
+       numb: 7,
+       question: "Nor in my _____ can I take delight",
+       answer: "Book",
+       options: [
+       "Book",
+       "Learning bower",
+       "Dreary shower",
+       "All of the Above"
+     ]
+   },
+
+      {
+       numb: 8,
+       question: "What drives all the joy away?",
+       answer: "School on a summer morning",
+       options: [
+        "Caged bird",
+        "Nip in the bud",
+        "Blown blossoms",
+        "School on a summer morning"
+     ]
+   },
+     
+     {
+       numb: 9,
+       question: "What does the boy like about the huntsman?",
+       answer: "Sound of his horn",
+       options: [
+           "Sound of his horn",
+           "Hunting skills",
+           "His personality",
+           "All of the Above"
+     ]
+   },
+    
+     {
+       numb: 10,
+       question: "Who is the poet of the poem “The School Boy?",
+       answer: "William Blake",
+       options: [
+           "William Blake",
+           "Thomas Wilde",
+           "Rabindranath Tagore",
+           "T.S. Eliot"
+     ]
+   },
+    
+    
+
+ ];
